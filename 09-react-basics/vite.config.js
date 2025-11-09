de2628c1b0b8ca75038ba1244e5b5683245fd76f
mw5762a <143518810+mw5762a@users.noreply.github.com>
2025-11-09 17:14:48 -0500

    live

--- 09-react-basics/vite.config.js
+++ 09-react-basics/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/csc336-fall2025/09-react-basics-live/',  
+  base: '/csc336-fall2025/09-react-basics-live/', // 👈 this is where it lives on GitHub Pages
 })
